28c8f6395a136cf8bc5528c6e5535b06ee45dc41
Renan Gustavo <renangustavoo101@gmail.com>
2019-10-22 18:17:48 -0300

    1

---
+++ TDIG_Atividade1/build/classes/classes/TelaPrincipal.rs
@@ -0,0 +1,10 @@
+view.TelaPrincipal$9
+view.TelaPrincipal$8
+view.TelaPrincipal$7
+view.TelaPrincipal$6
+view.TelaPrincipal$5
+view.TelaPrincipal$4
+view.TelaPrincipal$3
+view.TelaPrincipal$2
+view.TelaPrincipal$1
+view.TelaPrincipal
